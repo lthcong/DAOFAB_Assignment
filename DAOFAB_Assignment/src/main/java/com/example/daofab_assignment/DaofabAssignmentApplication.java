390522f673cb615362b62412c02f4688a65342f8
Cong La <lthcongit@gmail.com>
2021-08-14 15:34:32 +0700

    Initial commit.

---
+++ DAOFAB_Assignment/src/main/java/com/example/daofab_assignment/DaofabAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.daofab_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaofabAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaofabAssignmentApplication.class, args);
+    }
+
+}
---
+++ DAOFAB_Assignment/src/test/java/com/example/daofab_assignment/DaofabAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daofab_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaofabAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
